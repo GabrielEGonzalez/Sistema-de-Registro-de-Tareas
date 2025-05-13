5deea9646207c3165b9b5b0bbdd00254f44e6e03
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-05-12 21:39:56 -0500

    tabla index

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+$data = 'data/data.json';
+$datos_json = file_get_contents($data);
+$datos = json_decode($datos_json, true);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,6 +14,33 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+        <style>
+            table,thead , tbody , tr , td , th{
+                padding: .7em;
+            }
+
+            .contenedor2{
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%,-50%);
+                background-color: beige;
+                width: 100%;
+                padding: 2em;
+            }
+            
+            table{
+                width: 100%;
+                text-align: center;
+            }
+
+            thead{
+                text-align: center;
+                color: #000;
+                background-color: cadetblue;
+                border: 1px solid blue;
+            }
+        </style>
     <title>inicio</title>
 </head>
 
@@ -16,27 +50,45 @@
             <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="./public/add.php">agregar</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
         </ul>
     </div>
 
-    <?php
-    $data = 'data/data.json';
+    <div class="contenedor2">
+        <table>
 
-    $datos_json = file_get_contents($data);
-    $datos = json_decode($datos_json, true);
-    echo '<pre>';
-    var_dump($datos);
-    echo '</pre>';
-    ?>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Descripcion</th>
+                    <th>fecha limite</th>
+                    <th>Estado</th>
+                    <th>Opciones</th>
+                </tr>
+
+            </thead>
+            <tbody>
+                <?php foreach ($datos as $id => $dato): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($dato['nombre']) ?></td>
+                        <td><?php echo htmlspecialchars($dato['descripcion']) ?></td>
+                        <td><?php echo htmlspecialchars($dato['fecha_limite']) ?></td>
+                        <td><?php echo htmlspecialchars($dato['estado']) ?></td>
+                        <td>
+                            <div>
+                                <a href="./public/edit.php?codigo=<?php echo $id?>">editar</a>
+                                <a href="./public/delete.php?codigo=<?php echo $id?>">eliminar</a>
+                                <a href="./public/estado.php?codigo=<?php echo $id?>">estado</a>
+                            </div>
+                        </td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+
+        </table>
+
+    </div>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
