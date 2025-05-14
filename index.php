cd6353bc74fd7ae3d12c59a41e7396a51655cc25
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-05-14 10:09:13 -0500

    nuevo edit.php

--- index.php
+++ index.php
@@ -11,9 +11,7 @@ $datos = json_decode($datos_json, true);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    
         <style>
             table,thead , tbody , tr , td , th{
                 padding: .7em;
@@ -26,7 +24,6 @@ $datos = json_decode($datos_json, true);
                 transform: translate(-50%,-50%);
                 background-color: beige;
                 width: 100%;
-                padding: 2em;
             }
             
             table{
@@ -88,10 +85,6 @@ $datos = json_decode($datos_json, true);
 
     </div>
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ public/edit.php
@@ -0,0 +1,89 @@
+<?php
+$data = '../data/data.json';
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (isset($_GET['codigo'])) {
+        $codigo = $_GET['codigo'];
+    }
+
+    $datosString = file_get_contents($data);
+
+    if (empty($datos)) {
+        $datos = json_decode($datosString,true);
+    }
+
+    $datosUSER = $datos[$codigo];
+} else if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    /*
+    Funcionamiento : se obtiene en get el codigo de index para los datos , se obtiene los datos enviados pr el metodo post que se ran datos actualizado y nuevos , 
+     */
+    $Index = $_GET['codogo'];
+    $nuevosDatos = [
+        'nombre' => isset($_POST['nombre']) ? trim($_POST['nombre']) : '',
+        'descripcion' => isset($_POST['descripcion']) ? trim($_POST['descripcion']) : '',
+        'fecha_limite' => isset($_POST['fechaL']) ? trim($_POST['fechaL']) : '',
+        'estado' => isset($_POST['estado']) ? trim($_POST['estado']) : '',
+        'fecha_creacion' => isset($_POST['fechaC']) ? trim($_POST['fechaC']) : '',
+    ];
+
+    $datos = file_get_contents($data);
+    if (empty($datos)) {
+        $datosN = json_decode($datos,true);
+    }
+    $datosN['codigo'] = $nuevosDatos;
+    file_put_contents($data,$datosN,JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+}else{
+    echo 'ERROR';
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <div>
+            <h1>Edicion de Registro de tarea</h1>
+        </div>
+        <div class="con">
+            <div class="lcon">
+                <label for="nombre">Nombre:</label>
+                <input type="text" name="nombre" id="nombre" value="<?php echo htmlspecialchars($datosUSER["nombre"]);?>">
+            </div>
+            <div class="lcon">
+                <label for="descripcion">Descripcion:</label>
+                <textarea name="descripcion" id="descripcion"><?php echo htmlspecialchars($datosUSER["descripcion"]);?></textarea>
+            </div>
+            <div class="lcon">
+                <label for="fechaL">Fecha Limite:</label>
+                <input type="date" name="fechaL" id="fechaL" value="<?php echo htmlspecialchars($datosUSER["fecha_limite"]);?>">
+            </div>
+            <div class="lcon">
+                <label for="estado">Estado:</label>
+                <select name="estado" id="estado" value="<?php echo htmlspecialchars($datosUSER["estado"]);?>">
+                    <option value="Pendiente">Pendiente</option>
+                    <option value="En Progreso">En Progreso</option>
+                    <option value="Completada">Completada</option>
+                </select>
+            </div>
+            <div class="lcon">
+                <label for="fechaC">Fecha de Creacion:</label>
+                <input type="date" name="fechaC" id="fechaC" value="<?php echo htmlspecialchars($datosUSER["fecha_creacion"]);?>">
+            </div>
+            <div class="lcon">
+                <input type="submit" value="Enviar">
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
